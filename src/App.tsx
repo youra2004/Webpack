f12c98ba1048c82129fa664410c2999281079031
youra2004 <youra0305@gmail.com>
2023-08-29 14:52:48 +0300

    husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import './style.css'
 import image from './image.png'
 
-const App = () => {
-  const test = "TEST"
-  return (
-    <div>
-      <h1>React Typescript - {process.env.name}</h1>
-      <img src={image} alt="img" />
-    </div>
-  )
-}
+const App = () => (
+  <div>
+    <h1>React Typescript - {process.env.name}</h1>
+    <img src={image} alt="img" />
+  </div>
+)
 
 export default App
